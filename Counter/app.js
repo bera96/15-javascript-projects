8cc7951aa592d71bdda6fa29c648d32e88245be9
Bera Yilmazyurt <sentinelprime33@gmail.com>
2021-07-27 15:06:59 +0300

    Proje oluşturuldu

---
+++ Counter/app.js
@@ -0,0 +1,33 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.target.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        }
+        else if (styles.contains("reset")) {
+            count = 0;
+        }
+        else if (styles.contains("increase")) {
+            count++;
+        }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        else if (count === 0) {
+            value.style.color = "black";
+        }
+        else if (count < 0) {
+            value.style.color = "red";
+        }
+        value.textContent = count;
+    });
+
+});
